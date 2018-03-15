4675d0142e1718c961e388f2ed119691d4fb90e0
navadeepd <navdeepchowdary@gmail.com>
2018-03-15 19:32:45 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
-yum -y install  wget
-wget https://download.docker.com/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
-yum -y install docker-ce
+sudo yum -y install  wget
+sudo wget https://download.docker.com/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
+sudo yum -y install docker-ce
